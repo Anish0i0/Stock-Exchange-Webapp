844b964447fa3140786f9917acbbecb36ccd2e1b
Anish <anishdungdung248@gmail.com>
2026-05-17 16:49:48 +0530

    Fixed login page

--- frontend/src/landing_page/login/Login.jsx
+++ frontend/src/landing_page/login/Login.jsx
@@ -12,7 +12,7 @@ const Login = () => {
 
     try {
       const { data } = await axios.post(
-        `${import.meta.env.VITE_API_URL}/signup`,
+        `${import.meta.env.VITE_API_URL}/login`,
         {
           email,
           password,
